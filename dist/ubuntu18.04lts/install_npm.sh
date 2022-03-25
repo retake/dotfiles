8d758f14242f8f6e013a08d032cb6df698154181
retake <retake272@gmail.com>
2022-03-25 22:23:05 +0900

    npmのインストールスクリプトを修正

--- dist/ubuntu18.04lts/install_npm.sh
+++ dist/ubuntu18.04lts/install_npm.sh
@@ -1,4 +1,8 @@
 #! /bin/bash -x
 
 sudo apt update
-sudo apt install npm yarn -y
+sudo apt install nodejs npm yarn -y
+sudo npm install -g n -y
+sudo n stable -y
+sudo apt purge nodejs npm -y
+exec $SHELL -l
