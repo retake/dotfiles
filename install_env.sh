1ea27ab888d6444eb285f3796502788742f8b347
retake <retake272@gmail.com>
2022-03-29 15:47:31 +0900

    環境構築スクリプトを修正

--- install_env.sh
+++ install_env.sh
@@ -2,13 +2,18 @@
 
 cd $(dirname $0)
 
+# 必要なファイルのリンクを作成
 for file in .??*
 do
   [ "${file}" = ".git" ] && continue
   [ "${file}" = ".rbenv" ] && continue
+  [ "${file}" = "README.md" ] && continue
 
   ln -snfv $(pwd)/${file} ~/${file}
 done
+
+# 必要なディレクトリのリンクを作成
+ln -snfv $(pwd)/.vim ~/.vim
 
 if [ ! -d ~/bin ];then
   mkdir ~/bin
