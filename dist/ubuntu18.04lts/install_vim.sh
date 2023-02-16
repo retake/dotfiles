f58e815126107e9235881916d04102780abc86f1
retake <retake272@gmail.com>
2023-02-16 11:45:17 +0900

    vimのインストール方法を変える

--- dist/ubuntu18.04lts/install_vim.sh
+++ dist/ubuntu18.04lts/install_vim.sh
@@ -1,5 +1,14 @@
 #! /bin/bash -eux
 
-sudo add-apt-repository ppa:jonathonf/vim
-sudo apt update
-sudo apt install -y vim-gtk
+#sudo add-apt-repository ppa:jonathonf/vim
+#sudo apt update
+#sudo apt install -y vim-gtk
+
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge --enable-gui=gtk2 --enable-fail-if-missing
+make
+sudo make install
+cd ..
+rm -rf vim
+
