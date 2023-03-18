f14d759af4e24ad21e50d05e0683a7c9112ccf37
retake <retake272@gmail.com>
2023-03-18 09:51:02 +0900

    リファクタリング：変数名を修正

--- install_env.sh
+++ install_env.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -ex
 
 create_symlink() {
-  local src=$1
-  local dest=$2
-  ln -snfv "${src}" "${dest}"
+  local source_path=$1
+  local target_path=$2
+  ln -snfv "${source_path}" "${target_path}"
 }
 
 cd $(dirname $0)
 
 # 必要なファイルのリンクを作成
-for file in .??*; do
-  [ "${file}" = ".git" ] && continue
-  [ "${file}" = ".github" ] && continue
-  [ "${file}" = ".rbenv" ] && continue
+for dotfile in .??*; do
+  [ "${dotfile}" = ".git" ] && continue
+  [ "${dotfile}" = ".github" ] && continue
+  [ "${dotfile}" = ".rbenv" ] && continue
 
-  create_symlink $(pwd)/${file} ${HOME}/${file}
+  create_symlink $(pwd)/${dotfile} ${HOME}/${dotfile}
 done
 
 # 必要なディレクトリのリンクを作成
@@ -26,7 +26,7 @@ fi
 
 cd bin
 
-for script in ?*.sh; do
-  create_symlink $(pwd)/${script} ${HOME}/bin/${script}
+for shell_script in ?*.sh; do
+  create_symlink $(pwd)/${shell_script} ${HOME}/bin/${shell_script}
 done
 
