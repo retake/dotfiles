db026e4e9c22ac56dbdb33b7bccd65f1d389c975
retake <retake272@gmail.com>
2023-03-18 09:45:45 +0900

    リファクタリング：関数を使用

--- install_env.sh
+++ install_env.sh
@@ -1,27 +1,32 @@
-#! /bin/bash -ex
+#!/bin/bash -ex
+
+create_symlink() {
+  local src=$1
+  local dest=$2
+  ln -snfv "${src}" "${dest}"
+}
 
 cd $(dirname $0)
 
 # 必要なファイルのリンクを作成
-for file in .??*
-do
+for file in .??*; do
   [ "${file}" = ".git" ] && continue
   [ "${file}" = ".github" ] && continue
   [ "${file}" = ".rbenv" ] && continue
 
-  ln -snfv $(pwd)/${file} ~/${file}
+  create_symlink $(pwd)/${file} ~/${file}
 done
 
 # 必要なディレクトリのリンクを作成
-ln -snfv $(pwd)/.vim ~/.vim
+create_symlink $(pwd)/.vim ~/.vim
 
-if [ ! -d ~/bin ];then
+if [ ! -d ~/bin ]; then
   mkdir ~/bin
 fi
 
 cd bin
-for script in ?*.sh
-do
-  ln -snfv $(pwd)/${script} ~/bin/${script}
+
+for script in ?*.sh; do
+  create_symlink $(pwd)/${script} ~/bin/${script}
 done
 
